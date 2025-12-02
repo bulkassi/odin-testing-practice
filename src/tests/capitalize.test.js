3a1827dabe3b2e83d5ad13d0d84fae417192fda6
bulkassi <158156178+bulkassi@users.noreply.github.com>
2025-12-02 23:44:25 +0300

    refactor: wrap tests in `describe` with function name

--- src/tests/capitalize.test.js
+++ src/tests/capitalize.test.js
@@ -1,56 +1,58 @@
 import { capitalize } from "../functions/capitalize";
 
-describe.each([
-  {
-    string: "alphabet",
-    capitalized: "Alphabet",
-  },
-  {
-    string: "sorting",
-    capitalized: "Sorting",
-  },
-  {
-    string: "brother",
-    capitalized: "Brother",
-  },
-])("Basic examples", ({ string, capitalized }) => {
-  test(`${string}`, () => {
-    expect(capitalize(string)).toBe(capitalized);
-  });
-});
-
-describe.each([
-  {
-    string: "$ello, world!",
-    capitalized: "$Ello, world!",
-  },
-  {
-    string: "?hello, world!",
-    capitalized: "?Hello, world!",
-  },
-  {
-    string: "  hello, world!",
-    capitalized: "  Hello, world!",
-  },
-])("Special case: first char is not a letter", ({ string, capitalized }) => {
-  test(`${string}`, () => {
-    expect(capitalize(string)).toBe(capitalized);
-  });
-});
-
-describe("Edge cases", () => {
-  test("Empty string", () => {
-    expect(capitalize("")).toBe("");
+describe("capitalize", () => {
+  describe.each([
+    {
+      string: "alphabet",
+      capitalized: "Alphabet",
+    },
+    {
+      string: "sorting",
+      capitalized: "Sorting",
+    },
+    {
+      string: "brother",
+      capitalized: "Brother",
+    },
+  ])("Basic examples", ({ string, capitalized }) => {
+    test(`${string}`, () => {
+      expect(capitalize(string)).toBe(capitalized);
+    });
   });
 
-  test("null and undefined", () => {
-    expect(() => capitalize(null)).toThrow();
-    expect(() => capitalize(undefined)).toThrow();
+  describe.each([
+    {
+      string: "$ello, world!",
+      capitalized: "$Ello, world!",
+    },
+    {
+      string: "?hello, world!",
+      capitalized: "?Hello, world!",
+    },
+    {
+      string: "  hello, world!",
+      capitalized: "  Hello, world!",
+    },
+  ])("Special case: first char is not a letter", ({ string, capitalized }) => {
+    test(`${string}`, () => {
+      expect(capitalize(string)).toBe(capitalized);
+    });
   });
 
-  test("Not a string", () => {
-    expect(() => capitalize(0)).toThrow();
-    expect(() => capitalize(["a", "b", "c"])).toThrow();
-    expect(() => capitalize({})).toThrow();
+  describe("Edge cases", () => {
+    test("Empty string", () => {
+      expect(capitalize("")).toBe("");
+    });
+
+    test("null and undefined", () => {
+      expect(() => capitalize(null)).toThrow();
+      expect(() => capitalize(undefined)).toThrow();
+    });
+
+    test("Not a string", () => {
+      expect(() => capitalize(0)).toThrow();
+      expect(() => capitalize(["a", "b", "c"])).toThrow();
+      expect(() => capitalize({})).toThrow();
+    });
   });
 });
